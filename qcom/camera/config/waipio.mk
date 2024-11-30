e6d6b9f78aac0e27892536477ab9570a43ebbaa9
Rinigus <rinigus.git@gmail.com>
2024-11-30 11:02:07 +0200

    Import changes from Sony copyleft release
    Sony DTS: https://github.com/sonyxperiadev/kernel-copyleft-dts
    Branch: 64.1.A.0.xxx
    Commit: c6f61c483ab1de556e6c891dedaab40196dfb337

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -1,19 +1,37 @@
-dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
-dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
-								waipio-camera-sensor-mtp.dtbo \
-								waipio-camera-sensor-cdp.dtbo \
-								waipio-camera-sensor-qrd.dtbo
+# select based on the target
+ifeq ($(CONFIG_MACH_SONY_PDX223),y)
+SOMC_TARGET_PRODUCT := pdx223
+endif
+ifeq ($(CONFIG_MACH_SONY_PDX224),y)
+SOMC_TARGET_PRODUCT := pdx224
+endif
 
-dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
-dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
-dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-qrd.dtbo
+SOMC_TARGET_OPERATOR := generic
+SOMC_PLATFORM := nagara
 
-dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
-dtbo-$(CONFIG_ARCH_CAPE) += cape-camera-sensor-mtp.dtbo \
-				cape-camera-sensor-cdp.dtbo \
-				cape-camera-sensor-qrd.dtbo
+SOMC_PRODUCT_OPERATOR = $(SOMC_TARGET_PRODUCT)_$(SOMC_TARGET_OPERATOR)
 
-dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera-sensor-mtp.dtbo \
-				ukee-camera-sensor-cdp.dtbo \
-				ukee-camera-sensor-qrd.dtbo
-dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera.dtbo
+dtbo-$(CONFIG_ARCH_WAIPIO)              += waipio-camera.dtbo
+dtbo-$(CONFIG_ARCH_WAIPIO)              += waipio-camera-overlay-v2.dtbo
+
+ifeq ($(SOMC_PLATFORM), nagara)
+
+ifeq ($(findstring pdx223, $(SOMC_TARGET_PRODUCT)), pdx223)
+
+dtbo-$(CONFIG_ARCH_WAIPIO)              += waipio-camera-sensor-nagara-pdx223.dtbo
+
+endif # ($(findstring pdx223, $(SOMC_TARGET_PRODUCT)), pdx223)
+
+ifeq ($(findstring pdx224, $(SOMC_TARGET_PRODUCT)), pdx224)
+
+dtbo-$(CONFIG_ARCH_WAIPIO)              += waipio-camera-sensor-nagara-pdx224.dtbo
+
+endif # ($(findstring pdx224, $(SOMC_TARGET_PRODUCT)), pdx224)
+
+else # ($(SOMC_PLATFORM), nagara)
+
+dtbo-$(CONFIG_ARCH_WAIPIO)              += waipio-camera-sensor-mtp.dtbo \
+                                        waipio-camera-sensor-cdp.dtbo \
+                                        waipio-camera-sensor-qrd.dtbo
+
+endif # ($(SOMC_PLATFORM), nagara)
